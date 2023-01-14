2df9ff6871b35e5a788396cf7449c2057e7b9c2e
Ivan <Sernaiv@yandex.ru>
2023-01-15 00:18:16 +0300

    создание массива из 10 позиций и нахождение позиции заданного числа

---
+++ Task5/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); //заполнение массива collection числами от 1 до 10
+        index++; // index=index+1;
+    }
+}
+void PrintArray(int[] col) //void - когда метод ничего не возвращает
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // -1 ставится для того, чтобы не было нулевой позиции при отсутствии элемента в массиве
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index ++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // создает массив с 10 случайными числами. По умолчанию они все нули
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
